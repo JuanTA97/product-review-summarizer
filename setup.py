52784a6e0f4de630a6a690a6d7d6c750e39b832b
JuanTA97 <juppalaciope@unal.edu.co>
2025-09-05 23:49:56 -0500

    First commit - initial version of OpenAI review summarizer

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Package installer"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="homework",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "python-dotenv",
+        "pandas",
+    ],
+)
+
